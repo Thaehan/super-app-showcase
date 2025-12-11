66d542c2881c762522ab4e032cd867d22bd5a1c3
datdv7 <datdv7@viettel.com.vn>
2025-12-11 14:23:28 +0700

    [Upgrade RN] 0.82.1

--- packages/dashboard/index.js
+++ packages/dashboard/index.js
@@ -1,8 +1,8 @@
 import {ScriptManager} from '@callstack/repack/client';
 import {AppRegistry} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import App from './src/App';
 import {name as appName} from './app.json';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Only set storage caching in production
 // @todo: fix this to be reliable in both dev and prod
